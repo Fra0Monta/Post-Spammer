f4e131aa264c73df231b5bac96c7bab17f449363
Fra0Monta <montalbetti.francesco.norberto@iisgalvanimi.edu.it>
2020-01-08 13:53:21 +0000

    angular-bootstrap

---
+++ angular-bootstrap/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
